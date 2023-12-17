e59c150f8e6c3d2ca02986ca7fae4a1cb301709d
Võ Thanh Tú <77402349+Anhtraihoa@users.noreply.github.com>
2023-12-18 06:42:22 +0700

    Delete all

--- admin/pages/pIndex.php
+++
@@ -1,1 +0,0 @@
-<h1>Chào mừng đến trang admin của Kely Computer</h1>
--- modules/xlDangXuat.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-    session_destroy();
-
-    include "../libs/DataProvider.php";
-    DataProvider::ChangeURL("../index.php");
-?>
